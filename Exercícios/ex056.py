a449d3bb157ac06d364d5a9b2410546ebdbe4d5c
Ricardo Silva <ricardinho.jd@hotmail.com>
2022-06-01 16:39:32 -0300

    Update ex056.py

--- Exercícios/ex056.py
+++ Exercícios/ex056.py
@@ -5,18 +5,22 @@ do grupo, qual é o nome do homem mais velho e quantas mulheres têm menos de 20
 cont_idade = 0
 mais_velho = 0
 nome_maisvelho = ''
+mulher_menor = 0
 for pessoas in range(1, 5):
     print('----- {}ª PESSOA -----'.format(pessoas))
     nome = str(input('Nome: ')).strip()
     idade = int(input('Idade: '))
     sexo = str(input('Sexo [M/F]: ')).strip()
     cont_idade += idade
-    if pessoa == 1 and sexo in 'Mm':
+    if pessoas == 1 and sexo in 'Mm':
         mais_velho = idade
         nome_maisvelho = nome
     if sexo in 'Mm' and idade > mais_velho:
         mais_velho = idade
         nome_maisvelho = nome
+    if sexo in 'Ff' and idade < 20:
+        mulher_menor += 1
 media = cont_idade / 4
 print('A média de idade do grupo é {}'.format(media))
 print('O homem mais velho tem {} anos e seu nome é {}'.format(mais_velho, nome_maisvelho))
+print('No grupo tem {} mulheres menores de 20 anos'.format(mulher_menor))
