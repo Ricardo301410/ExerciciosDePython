a14edbf1ada03f54ba02cc82fedc9fce280b3875
Ricardo Silva <ricardinho.jd@hotmail.com>
2022-06-03 22:38:49 -0300

    Update ex058.py

--- Exercícios/ex058.py
+++ Exercícios/ex058.py
@@ -1,2 +1,16 @@
 """Melhore o jogo do DESAFIO 28 onde o computador vai "pensar" em um número entre 0 e 10. Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer."""
 
+from random import randint
+computador = randint(0, 10)
+print('Sou seu computador... Acabei de pesar em um número entre 0 e 10. Tente adivinhar...')
+acertou = False
+con_palpite = 0
+while not acertou:
+    jogador = int(input('Em que número eu pensei? '))
+    con_palpite += 1
+    if jogador == computador:
+        acertou = True
+    else:
+        if jogador < computador:
+            print('Um pouco mais alto, tente novamente!')
+print('PARABÉNS, você acertou na {}ª tentativa.'.format(con_palpite))
