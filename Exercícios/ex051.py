2f0282e94c66c97783037196e55173dc410a87ed
Ricardo Silva <ricardinho.jd@hotmail.com>
2022-06-05 13:17:58 -0300

    Update ex051.py

--- Exercícios/ex051.py
+++ Exercícios/ex051.py
@@ -1,6 +1,4 @@
-"""Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa
-progressão."""
-
+"""Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão."""
 
 primeiro_termo = int(input('Informe o 1º termo da PA: '))
 razao = int(input('Informe a razão da PA: '))
