8ee8e42e584e64ccaa8fe6ee9d060724430433d3
Ricardo Silva <ricardinho.jd@hotmail.com>
2022-06-04 23:11:50 -0300

    Update ex059.py

--- Exercícios/ex059.py
+++ Exercícios/ex059.py
@@ -6,6 +6,36 @@
 [ 5 ] sair do programa
 Seu programa deverá realizar a operação solicitada em cada caso."""
 
-for v in range(1, 3):
-    valor_1 = float(input('Digite o {}º valor: '.format(v)))
-# valor_2 = float(input('Digite o 2º valor: '))
+valor_1 = int(input('Digite o 1º valor: '))
+valor_2 = int(input('Digite o 2º valor: '))
+opcao = 0
+while opcao != 5:
+    print('''Escolha uma opção
+[1] Somar
+[2] Multiplicar
+[3] Maior
+[4] Novos números
+[5] Sair do programa''')
+    opcao = int(input('Sua opção: '))
+    if opcao == 1:
+        soma = valor_1 + valor_2
+        print('A soma de {} e {} é {}'.format(valor_1, valor_2, soma))
+    elif opcao == 2:
+        multi = valor_1 * valor_2
+        print('A multiplicação de {} e {} é'.format(valor_1, valor_2, multi))
+    elif opcao == 3:
+        if valor_1 > valor_2:
+            maior = valor_1
+        elif valor_1 < valor_2:
+            maior = valor_2
+        print('O maior valor entre {} e {} é {}'.format(valor_1, valor_2, maior))
+    elif opcao == 4:
+        print('Informe os números novamente!')
+        valor_1 = int(input('Digite o 1º valor: '))
+        valor_2 = int(input('Digite o 2º valor: '))
+    elif opcao == 5:
+        print('Finalizando...')
+    else:
+        print('Opção inválida, tente novamente.')
+    print('-=' * 10)
+print('{:-^40}'.format(' Fim do programa '))
