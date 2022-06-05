9a21361a33f15b4772f60faa5d56ac4df8b2acc0
Ricardo Silva <ricardinho.jd@hotmail.com>
2022-06-05 01:33:15 -0300

    Update ex059.py

--- Exercícios/ex059.py
+++ Exercícios/ex059.py
@@ -6,6 +6,7 @@
 [ 5 ] sair do programa
 Seu programa deverá realizar a operação solicitada em cada caso."""
 
+from time import sleep
 valor_1 = int(input('Digite o 1º valor: '))
 valor_2 = int(input('Digite o 2º valor: '))
 opcao = 0
@@ -19,10 +20,10 @@ while opcao != 5:
     opcao = int(input('Sua opção: '))
     if opcao == 1:
         soma = valor_1 + valor_2
-        print('A soma de {} e {} é {}'.format(valor_1, valor_2, soma))
+        print('A soma de {} + {} é {}'.format(valor_1, valor_2, soma))
     elif opcao == 2:
         multi = valor_1 * valor_2
-        print('A multiplicação de {} e {} é'.format(valor_1, valor_2, multi))
+        print('A multiplicação de {} x {} é {}'.format(valor_1, valor_2, multi))
     elif opcao == 3:
         if valor_1 > valor_2:
             maior = valor_1
@@ -35,7 +36,9 @@ while opcao != 5:
         valor_2 = int(input('Digite o 2º valor: '))
     elif opcao == 5:
         print('Finalizando...')
+        sleep(1)
     else:
         print('Opção inválida, tente novamente.')
     print('-=' * 10)
+    sleep(2)
 print('{:-^40}'.format(' Fim do programa '))
