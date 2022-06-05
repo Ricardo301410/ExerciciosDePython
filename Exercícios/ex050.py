a56679c4cef31332ad4a5be27333df9897f0fccd
Ricardo Silva <ricardinho.jd@hotmail.com>
2022-06-05 13:17:46 -0300

    Update ex050.py

--- Exercícios/ex050.py
+++ Exercícios/ex050.py
@@ -1,5 +1,4 @@
-"""Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor
-digitado for ímpar, desconsidere-o."""
+"""Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o."""
 
 soma = 0
 cont = 0
