ca48283e67ead63b600e572985592216f60a2219
Ricardo Silva <ricardinho.jd@hotmail.com>
2022-06-05 13:18:31 -0300

    Update ex054.py

--- Exercícios/ex054.py
+++ Exercícios/ex054.py
@@ -1,5 +1,4 @@
-"""Crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre quantas pessoas ainda não atingiram
-a maioridade e quantas já são maiores."""
+"""Crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre quantas pessoas ainda não atingiram a maioridade e quantas já são maiores."""
 
 from datetime import date
 menor = 0
