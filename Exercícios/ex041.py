a9d614abebb0e1712aadeaa460a958aa624489c0
Ricardo Silva <ricardinho.jd@hotmail.com>
2022-06-05 13:16:05 -0300

    Update ex041.py

--- Exercícios/ex041.py
+++ Exercícios/ex041.py
@@ -1,10 +1,10 @@
-"""A Confederação Nacional de Natação precisa de um programa que leia o ano de nascimento de um atleta e mostre sua
-categoria, de acordo com a idade:
+"""A Confederação Nacional de Natação precisa de um programa que leia o ano de nascimento de um atleta e mostre sua categoria, de acordo com a idade:
 – Até 9 anos: MIRIM
 – Até 14 anos: INFANTIL
 – Até 19 anos: JÚNIOR
 – Até 25 anos: SÊNIOR
 – Acima de 25 anos: MASTER"""
+
 from datetime import date
 nasc = int(input('Informe o ano de nascimento do atleta: '))
 atual = date.today().year
