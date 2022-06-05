3f96f3ad043b3eb2bc488e13cd93b3aa5e8b7024
Ricardo Silva <ricardinho.jd@hotmail.com>
2022-06-05 13:14:55 -0300

    Update ex037.py

--- Exercícios/ex037.py
+++ Exercícios/ex037.py
@@ -1,5 +1,8 @@
 """Escreva um programa em Python que leia um número inteiro qualquer e peça para o usuário escolher qual será a base
-de conversão: 1 para binário, 2 para octal e 3 para hexadecimal."""
+de conversão:
+[ 1 ] para binário
+[ 2 ] para octal
+[ 3 ] para hexadecimal."""
 
 num = int(input('Digite um número: '))
 cor = {'verde': '\033[32m', 'azul': '\033[34m', 'vermelho': '\033[31m', 'limpa': '\033[m'}
