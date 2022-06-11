013c48b106eddda29293f7193e89036c4f7d0667
Ricardo Silva <ricardinho.jd@hotmail.com>
2022-06-11 20:36:53 -0300

    Create ex067.py

---
+++ Exercícios/ex067.py
@@ -0,0 +1,8 @@
+"""Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário. O programa será interrompido quando o número solicitado for negativo."""
+
+while True:
+    numero = int(input('Digite um número para ver sua tabuada: '))
+    if numero < 0:
+        break
+    for n in range(1, 11):
+        print(f'{numero} X {n:2} = {numero * n}')
