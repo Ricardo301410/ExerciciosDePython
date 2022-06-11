de4dcf271a542cfc5854449559d94c52217ed728
Ricardo Silva <ricardinho.jd@hotmail.com>
2022-06-11 20:07:39 -0300

    Update ex066.py

--- Exercícios/ex066.py
+++ Exercícios/ex066.py
@@ -2,7 +2,7 @@
 
 cont = soma = 0
 while True:
-    numero = int(input('Digite um número: '))
+    numero = int(input('Digite um número [999 para parar]: '))
     if numero == 999:
         break
     cont += 1
