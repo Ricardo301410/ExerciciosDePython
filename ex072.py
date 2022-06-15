925833655943549e0a13669e37d7a0a570180d50
Ricardo Silva <ricardinho.jd@hotmail.com>
2022-06-14 23:28:51 -0300

    Create ex072.py

---
+++ ex072.py
@@ -0,0 +1,9 @@
+"""Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de zero até vinte. Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso."""
+
+contagem = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
+while True:
+    numero = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= numero <= 20:
+        break
+    print('Tente novamente!', end=' ')
+print(f'Você digitou o número {contagem[numero]}')
