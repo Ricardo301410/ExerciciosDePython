b32fb4b036f6aff861795533c5a7fcf825301a91
Ricardo Silva <ricardinho.jd@hotmail.com>
2022-06-16 00:41:14 -0300

    Update ex073.py

--- Exercícios/ex073.py
+++ Exercícios/ex073.py
@@ -4,4 +4,15 @@ b) Os últimos 4 colocados.
 c) Times em ordem alfabética.
 d) Em que posição está o time da Chapecoense."""
 
-times = ('')
+times = ('Corinthians', 'Palmeiras', 'Santos', 'Grêmio', 'Cruzeiro', 'Flamengo', 'Vasco', 'Chapecoense', 'Atlético-MG', 'Botafogo', 'Atlético', 'Bahia', 'Sao Paulo', 'Fluminense', 'Sport', 'Vitória', 'Coritiba', 'Avaí', 'Ponte Preta', 'Atlético-GO')
+print('-=' * 40)
+print(f'Os 20 colocados do Brasileirão são: {times}')
+print('-=' * 40)
+print(f'Os 5 primeiros colocados são: {times[:5]}')
+print('-=' * 40)
+print(f'Os últimos 4 colocados são: {times[-4:]}')
+print('-=' * 40)
+print(f'Todos o colocados em ordem alfabética são: {sorted(times)}')
+print('-=' * 40)
+print(f'A Chapecoense está na {times.index("Chapecoense") + 1}ª posição')
+print('-=' * 40)
